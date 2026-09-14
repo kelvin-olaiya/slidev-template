92e9f2b1063665deb4211860c672800d76729fad
Kelvin Olaiya <kelvin.olaiya@unibo.it>
2026-09-14 14:53:20 +0200

    style of the presentation

---
+++ slide-bottom.vue
@@ -0,0 +1,17 @@
+<template>
+  <div v-if="$nav.currentLayout !== 'cover' && $nav.currentLayout !== 'intro'" class="slide-page-number">
+    {{ $nav.currentPage }} / {{ $nav.total }}
+  </div>
+</template>
+
+<style scoped>
+.slide-page-number {
+  position: absolute;
+  left: 24px;
+  bottom: 18px;
+  font-size: 0.75rem;
+  color: var(--brand-gray);
+  opacity: 0.7;
+  z-index: 10;
+}
+</style>
